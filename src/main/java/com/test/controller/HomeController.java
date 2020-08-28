2ceb76ddccb6a55a0e52f7ac3936a376371ce2a9
Ashwini3243 <ashwinirakam3243@gmail.com>
2020-08-28 17:13:48 +0530

    committing msg

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController
 {
     @RequestMapping("/home")
-    public String home()
+    public String home(Model model)
     {
+        model.addAttribute("msg","hpractice");
         return "home";
     }
 
--- src/main/webapp/WEB-INF/pages/home.jsp
+++ src/main/webapp/WEB-INF/pages/home.jsp
@@ -22,6 +22,6 @@
 
 </head>
 <body>
-
+${msg}
 </body>
 </html>
