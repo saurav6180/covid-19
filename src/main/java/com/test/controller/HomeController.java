8632c8cabf037965797f4aa80fd5c4cc83623f3e
sauravkr827 <sauravkr82711@gmail.com>
2020-08-31 10:21:06 +0530

    updated things commited

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.test.controller;
 
+import com.test.dao.CustomerDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController
 {
+    @Autowired
+    CustomerDaoImpl customerDao;
+
     @RequestMapping("/home")
-    public String home()
-    {
+    public String home(Model model)
+
+    {    model.addAttribute("data",customerDao.save());
         return "home";
     }
 
